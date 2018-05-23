5cc9963078550b3ee05570748d8cb73ae13bec17
joel123vilca <joevil23tjq@gmail.com>
2018-05-22 23:57:49 -0500

    estilos del primer componente

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,15 +1,22 @@
 import React,{Component} from 'react';
-
+import './media.css';
 class Media extends Component{
   render(){
+    const styles ={
+      container: {
+        color:'#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
     return (
-      <div>
-        <div>
-          <img
-          src=""
+
+      <div className="Media">
+        <div className="Media-cover">
+          <img className="Media-img"
+          src="./images/covers/clase1.jpeg"
           alt=""
-          with={260}
-          height={160}
           />
           <h3>por que aprender react</h3>
           <p>joel vilca</p>
