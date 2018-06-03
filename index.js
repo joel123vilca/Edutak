c56a408eedfeb65bf4d6050a7a27092c8b19d219
joel123vilca <joevil23tjq@gmail.com>
2018-06-02 20:41:43 -0500

    componentes con dos apis

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
  import React from 'react';
  //import ReactDOM from 'react-dom';
  import {render} from 'react-dom';
- // import Playlist from './src/playlist/components/Playlist';
- // import data from './src/api.json';
+ import Playlist from './src/playlist/components/Playlist';
+ import data from './src/api.json';
  import Profesores from './src/profesores/components/Profesores'
  import data1 from './src/profesores.json';
 
@@ -10,5 +10,8 @@
  //ReactDOM.render(que voy a renderizar);
  //ReactDOM.render(donde voya renderizar);
 const app = document.getElementById('app')
-render(<Profesores data = { data1 }/>, app);
+render(<Playlist data = {data} />, app);
+
+const profe = document.getElementById('profe')
+render(<Profesores data = {data1} />, profe);
 //<Media type="video" title="introduccion a Edutak" author="Joel Vilca" image="./images/covers/clase1.jpeg"/>,
