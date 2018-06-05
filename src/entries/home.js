84f66192099c1de4e0cf6c5c7444e3cd74082151
joel123vilca <joevil23tjq@gmail.com>
2018-06-05 10:56:59 -0500

    estructura con containers y UI componentes

---
+++ src/entries/home.js
@@ -0,0 +1,21 @@
+ import React from 'react';
+ //import ReactDOM from 'react-dom';
+ import {render} from 'react-dom';
+ //import Playlist from './src/playlist/components/Playlist';
+ import Home from '../pages/containers/home';
+ //import data from './src/api.json';
+ // import Profesores from './src/profesores/components/Profesores';
+ // import data1 from './src/profesores.json';
+ // import Precios from './src/precios/components/Precios';
+
+ //ReactDOM.render(que voy a renderizar);
+ //ReactDOM.render(donde voya renderizar);
+const app = document.getElementById('app')
+render(<Home/>, app);
+//
+// const profe = document.getElementById('profe')
+// render(<Profesores data = {data1} />, profe);
+//
+// const precios = document.getElementById('precios')
+// render(<Precios />, precios);
+//<Media type="video" title="introduccion a Edutak" author="Joel Vilca" image="./images/covers/clase1.jpeg"/>,
---
+++ src/pages/components/home-layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeLayout(props) {
+  return(
+    <section>
+    <h3>layout</h3>
+    { props.children}
+    </section>
+  )
+}
+
+export default HomeLayout
