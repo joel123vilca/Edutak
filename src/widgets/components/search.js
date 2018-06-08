e19cadda4f500f40b32879ddb8d8557367dd830c
joel123vilca <joevil23tjq@gmail.com>
2018-06-08 11:20:14 -0500

    UI formularios en el buscador

---
+++ src/widgets/components/search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './search.css';
+// function Search(props){
+//   return(
+//     <form action=""></form>
+//   )
+// }
+ const Search = () => (
+   (
+      <form  className="Search">
+      <input className="Search-input"
+      placeholder="busca tu curso favorito"
+      type="text"/>
+      </form>
+   )
+ )
+export default Search;
---
+++ src/widgets/containers/search.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Search from '../components/search';
+class SearchContainer extends Component {
+  render(){
+    return(
+        <Search/>
+    )
+  }
+}
+
+export default SearchContainer;
