fe804973c5d09d9da1a6ec7f399aba369f51b14b
joel123vilca <joevil23tjq@gmail.com>
2018-06-11 20:46:34 -0500

    componente de titulo y fuente de video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
+import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayer  extends Component{
   render(){
     return(
       <VideoPlayerLayout>
-      <video
-      controls
-      autoPlay
-      src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
-      />
+        <Title
+        title="Esto es un video chido!"
+        />
+        <Video
+        autoplay = {true}
+        src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
+        />
       </VideoPlayerLayout>
     )
   }
