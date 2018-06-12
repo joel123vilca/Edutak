b896b8e82533c48d499322c1ed65efce2bc5c7b9
joel123vilca <joevil23tjq@gmail.com>
2018-06-12 10:57:27 -0500

    duracion del video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,11 +2,14 @@ import React, { Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/Video';
 import Title from '../components/title';
-import PlayPause from '../components/play-pause'
+import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import Controls from '../components/video-player-controls.js';
 
 class VideoPlayer  extends Component{
   state = {
     pause: true,
+    duration: 0,
   }
   togglePlay = (event) => {
     this.setState({
@@ -18,19 +21,31 @@ class VideoPlayer  extends Component{
       pause:(!this.props.autoplay)
     })
   }
+  handleLoadedMetadata = event =>{
+    this.video = event.target;
+    this.setState({
+      duration:this.video.duration
+    })
+  }
   render(){
     return(
       <VideoPlayerLayout>
         <Title
         title="Esto es un video de aritmetica"
         />
-        <PlayPause
+        <Controls>
+          <PlayPause
           pause={this.state.pause}
           handleClick={this.togglePlay}
-        />
+          />
+          <Timer
+          duration={this.state.duration}
+          />
+        </Controls>
         <Video
         autoplay = {this.props.autoplay}
         pause={this.state.pause}
+        handleLoadedMetadata = {this.handleLoadedMetadata}
         src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
         />
 
