4420d53f9718eac8a87dcad8310c6794e0887e64
joel123vilca <joevil23tjq@gmail.com>
2018-06-11 21:20:11 -0500

    Play / pause : creando UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,18 +2,32 @@ import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer  extends Component{
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
   render(){
     return(
       <VideoPlayerLayout>
         <Title
-        title="Esto es un video chido!"
+        title="Esto es un video de aritmetica"
+        />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
         />
         <Video
         autoplay = {true}
         src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
         />
+
       </VideoPlayerLayout>
     )
   }
